bfe465817679a795fce62463400a8a3067d960ff
nishi <pisces.nisikawa@gmail.com>
2018-06-29 18:32:32 +0900

    fix-design

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/', to: 'tasks#index'
-  # get '/', to: 'tasks#new'
-  # post '/tasks', to: 'tasks#create'
   resources :tasks, only: [:create,:edit,:update,:toggle]
   root to: 'tasks#index', only: [:index]
   patch '/tasks/:id/toggle', to: 'tasks#toggle', as: 'toggle'
